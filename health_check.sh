c7494fd4b71951e23f2c96f40e5c5d3c9758214e
Rick <rickyang2910@gmail.com>
2022-10-22 23:00:01 +0800

    exercise 11.12. Custom health check

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+echo "Hello from shell script."
 
-exit 1 # exit status 1 means that the script "fails"
+exit 0  # exit status 0 means that the script "succeeds"
