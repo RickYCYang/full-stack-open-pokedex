70237e97cde6f892c856799604bbc62c3f80616c
Rick <rickyang2910@gmail.com>
2022-10-23 00:13:50 +0800

    exercise 11.12. Custom health check

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-echo "Hello from shell script."
+health_check=$(curl https://pokemon.fly.dev/health)
 
-exit 0  # exit status 0 means that the script "succeeds"
+if [ ${health_check} = "ok" ] ; then
+  echo "test ok"
+  exit 0  # exit status 0 means that the script "succeeds"
+else
+  echo "test fail"
+  exit 1 # exit status 1 means that the script "fails"
+fi
+
